08304de2eba04bb364f4a50532ec13614b204e12
RandyVald <radionburda@gmail.com>
2024-01-24 00:24:17 +0300

    add lesson1:task5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿int N = 100;
+int CurrentValue = 1;
+
+while (CurrentValue <= N)
+{
+    //Console.WriteLine(CurrentValue);
+    Console.Write(CurrentValue);
+    Console.Write(' ');
+    CurrentValue = CurrentValue + 1;
+}
